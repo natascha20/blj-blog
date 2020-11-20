25f9eda4dad7609e17aab25c6323f834fab397a5
Natascha Müller <natascha@Nataschas-MacBook-Pro.local>
2020-11-20 15:50:56 +0100

    commit 1

--- login.php
+++ login.php
@@ -1,3 +1,23 @@
 <?php 
         include "nav.php";
         ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/styles.css">
+        <title>Document</title>
+</head>
+<body>
+<main>
+        <form action="post" class="login">
+                <label for="User">Username</label>
+                <input type="text" name="User"> <br>
+                <label for="Password">Password</label>
+                <input type="password" name="Password"><br>
+        </form>    
+</main>
+</body>
+</html>
--- view.php
+++ view.php
@@ -2,59 +2,46 @@
 
 include("logic.php");
 
-// senden des Formulars
-
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
-    <title>Blog</title>
-</head>
-<body>
-    <?php 
-        include "nav.php";
-        ?>
-    <main>
-        <h1>Blog</h1>
-        
-            <!-- <?php
-                foreach($statement->fetchAll() as $task){
-                    echo'<li>' .$task["title"] . '<li>';
-                }
-            ?> -->
-
-        <ul>
-            <li>Beispielbeitrag</li>
-            <li><img src="img/sunset.jpg" alt="sunset"></li>
-            <li>Beispielbeitrag1 mit mehr Text</li>
-            <li>Beispielbeitrag2 Was habe ich heute gemacht: xxxxx</li>
-            <li>Beispielbeitrag3 xxxxxxxxxxxxxxxxxxxx <br> yyyyyyyyyyyy</li>
-            <li><img src="img/travel.jpg" alt="travel"></li>
-            <li>Beispielbeitrag4</li>
-            <li><img src="img/ocan.jpg" alt="ocan"></li>
-        </ul>
-
-        <div class="form">
-            <form action="view.php" method="post">
-                <label for="form">Your Post:<br></label>
-                <input type="text" name="username" id="username" value="<?=$username?>"><br />
-                <input type="text" name="postTitle" value="<?=$postTitle?>"><br />
-                <textarea name="postText" id="postText" cols="30" rows="5" value="<?=$postText?>"></textarea><br />
-                <input type="Submit" value="Absenden" />
-            </form>
-            <?php 
-                $username = $_POST['username'];
-                $postTitle = $_POST["postTitle"];
-                $postText = $_POST["postText"];
-                $pdo->query("INSERT INTO `posts`(created_by, created_at, post_title, post_text) VALUES ($username,time,$postTitle,$postText)");
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/styles.css">
+        <title>Blog</title>
+    </head>
+    <body>
+        <?php 
+            include "nav.php";
             ?>
-        </div>
-        
-    </main>
-</body>
+        <main>
+            <h1>Blog</h1>
+            <ul>
+                <li>Beispielbeitrag</li>
+                <li><img src="img/sunset.jpg" alt="sunset"></li>
+                <li>Beispielbeitrag1 mit mehr Text</li>
+                <li>Beispielbeitrag2 Was habe ich heute gemacht: xxxxx</li>
+                <li>Beispielbeitrag3 xxxxxxxxxxxxxxxxxxxx <br> yyyyyyyyyyyy</li>
+                <li><img src="img/travel.jpg" alt="travel"></li>
+                <li>Beispielbeitrag4</li>
+                <li><img src="img/ocan.jpg" alt="ocan"></li>
+            </ul>
+            <div class="form">
+                <form action="view.php" method="post">
+                    <label for="form">Your Post:<br></label>
+                    <input type="text" name="username" value="<?=$username?>"><br>
+                    <input type="text" name="postTitle" value="<?=$postTitle?>"><br>
+                    <textarea name="postText" id="postText" cols="30" rows="5" value="<?=$postText?>"></textarea><br/>
+                    <input type="Submit" value="Absenden"> <br>
+                </form>
+                <?php 
+                $stmt = $pdo->prepare("INSERT INTO posts (created_by, post_title, post_text) VALUES(now(), :creator, :title, :post)");
+                $stmt->execute([':creator' => $username, ':title' => $postTitle, ':post' => $postText]);
+                ?>
+            </div>
+        </main>
+    </body>
 </html>
